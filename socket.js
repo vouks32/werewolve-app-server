118b80942f0030d141ccb9ef5bcfa6aa678bee6e
Vouks <jotsaange@gmail.com>
2025-11-14 18:19:26 +0100

    uuuu

--- socket.js
+++ socket.js
@@ -18,6 +18,13 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE"]
   }));
 
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 

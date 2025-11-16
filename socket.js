bea40cd5160089beeb541a8fbb63ce327cd7f2ef
Vouks <jotsaange@gmail.com>
2025-11-17 00:28:01 +0100

    up

--- socket.js
+++ socket.js
@@ -20,7 +20,7 @@ export class AppSocket {
         this.app.use((req, res, next) => {
             res.header("Access-Control-Allow-Origin", "*");
             res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-            res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+            res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, skip_zrok_interstitial");
             if (req.method === "OPTIONS") {
                 return res.sendStatus(200);
             }

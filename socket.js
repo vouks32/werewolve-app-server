16aff907cb71d8c36f659aeee960edd0899130a3
Vouks <jotsaange@gmail.com>
2025-11-17 00:44:43 +0100

    lnbn

--- socket.js
+++ socket.js
@@ -57,7 +57,7 @@ export class AppSocket {
             console.error('Server error:', error);
         });
 
-        const io = Server(this.server)
+        const io = new Server(this.server)
 
         io.on('connection', (socket) => {
             console.log('A user connected');

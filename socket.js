8603b5a4c0d74e1d34fe44c9c1f51e88e450f5e9
Vouks <jotsaange@gmail.com>
2025-11-17 01:08:34 +0100

    kjg

--- socket.js
+++ socket.js
@@ -60,7 +60,7 @@ export class AppSocket {
             }
         });
 
-        io.on('connection', (socket) => {
+        this.io.on('connection', (socket) => {
             console.log('A user connected');
 
             socket.on('chat message', (msg) => {
